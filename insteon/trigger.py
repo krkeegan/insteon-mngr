f36059dd9e8c1d26a0af00ba83a2645a943857e5
KRKeegan <kevin@krkeegan.com>
2017-01-16 21:33:37 -0800

    Add Concept of Insteon Msg Type To Trigger

--- insteon/trigger.py
+++ insteon/trigger.py
@@ -13,24 +13,27 @@ class Trigger_Manager(object):
     # TODO remove expired triggers?
 
     def match_msg(self, msg):
-        haystack = msg.parsed_attributes
-        matched_keys = []
-        for trigger_key, trigger in self._triggers.items():
-            needle = trigger.attributes
-            trigger_match = True
-            for test_key, test_val in needle.items():
-                if ((test_key in haystack) and
-                        (needle[test_key] != haystack[test_key])):
-                    trigger_match = False
-                    break
-            if (trigger_match):
-                matched_keys.append(trigger_key)
-        for trigger_key in matched_keys:
-            # Delete trigger before running, to allow reusing same trigger_key
-            trigger = self._triggers[trigger_key]
-            trigger_function = trigger.trigger_function
-            del self._triggers[trigger_key]
-            trigger_function()
+        if msg.allow_trigger:
+            haystack = msg.parsed_attributes
+            if msg.insteon_msg:
+                haystack['insteon_msg_type'] = msg.insteon_msg.message_type
+            matched_keys = []
+            for trigger_key, trigger in self._triggers.items():
+                needle = trigger.attributes
+                trigger_match = True
+                for test_key, test_val in needle.items():
+                    if ((test_key in haystack) and
+                            (needle[test_key] != haystack[test_key])):
+                        trigger_match = False
+                        break
+                if trigger_match:
+                    matched_keys.append(trigger_key)
+            for trigger_key in matched_keys:
+                # Delete trigger before running, to allow reusing same trigger_key
+                trigger = self._triggers[trigger_key]
+                trigger_function = trigger.trigger_function
+                del self._triggers[trigger_key]
+                trigger_function()
 
     def run_trigger(self, msg, trigger_key):
         trigger = self._triggers[trigger_key]
@@ -44,7 +47,8 @@ class Trigger(object):
 
     def __init__(self, attributes={}):
         '''Trigger functions will be called when a message matching all of the
-        identified attributes is received the trigger is then deleted.'''
+        identified attributes is received the trigger is then deleted.
+        insteon_msg_type is a special attribute'''
         self._msg_attributes = attributes
         self._trigger_function = lambda: None
 
